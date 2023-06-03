b6b1b44910b554dc7c2e1d8dd27a2c3c62b17e9d
Wayne Koorts <wayne@wkoorts.com>
2023-06-04 01:53:41 +1200

    Add TODO.

--- Scratchy/ScratchyToolWindow.cs
+++ Scratchy/ScratchyToolWindow.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.Shell;
+﻿using System.Diagnostics;
+using System.Globalization;
+using Microsoft.VisualStudio.Shell;
 using System.Runtime.InteropServices;
 
 namespace Scratchy
@@ -23,6 +25,7 @@ namespace Scratchy
         public ScratchyToolWindow() : base(null)
         {
             Caption = "Scratchy";
+            // TODO: Check if a solution is currently loaded.
 
             // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
             // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
